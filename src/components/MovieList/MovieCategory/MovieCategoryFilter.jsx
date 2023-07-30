3eaf75d84a20aa89799ac70884d8eb92d8fb0409
Serah-Wanjiru <wanjirumburuuu@gmail.com>
2023-07-30 11:26:53 -0400

    first commit

--- src/components/MovieList/MovieCategory/MovieCategoryFilter.jsx
+++ src/components/MovieList/MovieCategory/MovieCategoryFilter.jsx
@@ -14,14 +14,12 @@ const CategoryFilter = ({ selectedCategory, handleCategoryChange }) => {
     { id: "14", name: "Fantasy" },
     { id: "36", name: "History" },
     { id: "27", name: "Horror" },
-    { id: "10402", name: "Music" },
     { id: "9648", name: "Mystery" },
     { id: "10749", name: "Romance" },
     { id: "878", name: "Science Fiction" },
     { id: "10770", name: "TV Movie" },
     { id: "53", name: "Thriller" },
-    { id: "10752", name: "War" },
-    { id: "37", name: "Western" },
+   
   ];
 
   return (
